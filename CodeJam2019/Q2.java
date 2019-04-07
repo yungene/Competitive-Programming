53c7b70994639dfa3526e03bcf407a3050c04f95
Jevgenijus <32654121+yungene@users.noreply.github.com>
2019-04-07 23:30:59 +0100

    Update Q2.java
    You Can Go Your Own Way

--- CodeJam2019/Q2.java
+++ CodeJam2019/Q2.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-public class B2 {
+//You Can Go Your Own Way
+public class Q2 {
 	static Reader reader = new Reader();
 
 	public static void main(String[] args) {
